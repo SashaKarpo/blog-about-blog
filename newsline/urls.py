0004a3735651ac23d5ad2c5113b35e1234308ce0
Alexandr <115651139+SashaKarpo@users.noreply.github.com>
2023-12-09 15:31:20 +0300

    added test func and urls

--- newsline/urls.py
+++ newsline/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, register_converter
 from . import views
+from . import converters
+
+
+register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home'),
+    path('post/', views.post),
+    path('archive/<year4:year>/', views.archive),
 ]
--- newsline/views.py
+++ newsline/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound
 
 
 def index(request):
     return HttpResponse('Cnhfyb')
 
+
+def post(request):
+    return HttpResponse('post')
+
+
+def archive(request, year):
+    if year > 2023:
+        return redirect('home', permanent=True)
+
+    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+
+
 # Create your views here.
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')

41ba7c41a45e03e82d44a82108f5517dd35d0761
Саша <prmzwe@gmail.com>
2024-01-22 14:15:35 +0300

    add links menu

--- newsline/urls.py
+++ newsline/urls.py
@@ -6,7 +6,9 @@ register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('post/', views.post),
+    path('post/<int:post_id>', views.show_post, name="post"),
+    path('addpage/', views.addpage, name='add_page'),
+    path('login/', views.login, name='login'),
+    path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
-    path('archive/<year4:year>/', views.archive),
 ]
--- newsline/views.py
+++ newsline/views.py
@@ -2,25 +2,45 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound
 from django.template.loader import render_to_string
 
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
+
 
 def index(request):
-    # item = render_to_string('newsline/index.html')
-    return render(request, 'newsline/index.html')
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'posts': data_db,
+    }
+    return render(request, 'newsline/index.html', context=data)
 
 
 def about(request):
     return render(request, 'newsline/about.html')
 
 
-def post(request):
-    return HttpResponse('post')
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с id={post_id}')
 
 
-def archive(request, year):
-    if year > 2023:
-        return redirect('home', permanent=True)
+def login(request):
+    return HttpResponse('Войти')
 
-    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+
+def addpage(request):
+    return HttpResponse('Добавить статью')
 
 
 # Create your views here.

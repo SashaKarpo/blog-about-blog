903cf44ec0674cc9a699e66e28cf457c89501c41
Саша <prmzwe@gmail.com>
2024-02-24 19:32:13 +0300

    Configured display of the list of articles

--- newsline/admin.py
+++ newsline/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'time_create', 'is_published', 'cat')
+    list_display_links = ('id', 'title')
+    ordering = ('title',)
+    list_editable = ('is_published', )
+    list_per_page = 10
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+
+# admin.site.register(Post, PostAdmin)
 
 # Register your models here.
--- newsline/models.py
+++ newsline/models.py
@@ -12,15 +12,16 @@ class Post(models.Model):
         DRAFT = 0, 'Черновик'
         PUBLISHED = 1, 'Опубликовано'
 
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(choices=Status.choices, default=Status.PUBLISHED)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts')
-    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags')
-    husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True,  related_name='woman')
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='Слаг')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
+    is_published = models.IntegerField(choices=Status.choices, default=Status.PUBLISHED, verbose_name='Публикация')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts', verbose_name='Категория')
+    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags', verbose_name='Теги')
+    husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True, related_name='woman',
+                                   verbose_name='Муж')
 
     objects = models.Manager()
     published = PublishedManager()
@@ -41,11 +42,15 @@ class Post(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_slug': self.slug})

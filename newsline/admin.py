bc10c4bbd7244744bf022aef47d7f512aeca6eeb
Саша <prmzwe@gmail.com>
2024-03-01 11:22:29 +0300

    added custom fields to the admin panel

--- newsline/admin.py
+++ newsline/admin.py
@@ -1,14 +1,31 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+
+
 from .models import Post, Category
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'content', 'time_create', 'is_published', 'cat')
-    list_display_links = ('id', 'title')
+    list_display = ('title', 'content', 'time_create', 'is_published', 'cat', 'brief_info')
+    list_display_links = ('title',)
     ordering = ('title',)
-    list_editable = ('is_published', )
+    list_editable = ('is_published',)
     list_per_page = 10
+    actions = ('set_published', 'set_draft')
+
+    @admin.display(description='Краткое описание', ordering='content')
+    def brief_info(self, post: Post):
+        return f'Описание {len(post.content)} символов'
+
+    @admin.action(description='Опубликовать выбранные записи')
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=Post.Status.PUBLISHED)
+        self.message_user(request, f'Опубликовано {count} статей')
+
+    @admin.action(description='Снять с публикации выбранные записи')
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Post.Status.DRAFT)
+        self.message_user(request, f'Добавлено в черновик {count} статей', messages.WARNING)
 
 
 @admin.register(Category)

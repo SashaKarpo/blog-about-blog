07da83abe3cda8cdac94cbe0d2a4ebd6193aed40
Саша <prmzwe@gmail.com>
2024-03-04 16:06:47 +0300

    Made the form fields display. Saving transferred data to the database

--- newsline/forms.py
+++ newsline/forms.py
@@ -3,9 +3,9 @@ from .models import Husband, Category
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea())
-    is_published = forms.BooleanField(required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
-    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False)
+    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    slug = forms.SlugField(max_length=255, label='Slug')
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows':5}), label='Содержимое')
+    is_published = forms.BooleanField(required=False, label='Опубликовать?', initial=True)
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label='Категория не выбрана', label='Категория')
+    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False, label='Муж', empty_label='Не замужем')
--- newsline/views.py
+++ newsline/views.py
@@ -64,7 +64,12 @@ def addpage(request):
     if request.method == 'POST':
         form = AddPostForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            # print(form.cleaned_data)
+            try:
+                Post.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления записи')
     else:
         form = AddPostForm()
 

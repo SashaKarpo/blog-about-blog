9cd9152f16aa00ec294b2252014fb5b102dc4bd4
Саша <prmzwe@gmail.com>
2024-03-11 15:37:57 +0300

    added class DataMixin

--- newsline/views.py
+++ newsline/views.py
@@ -8,34 +8,15 @@ from django.views.generic import TemplateView, ListView, DetailView, FormView, C
 
 from newsline.forms import AddPostForm, UploadFileForm
 from newsline.models import Post, Category, TagPost, UploadFiles
-
-menu = [{'title': "О сайте", 'url_name': 'about'},
-        {'title': "Добавить статью", 'url_name': 'add_page'},
-        {'title': "Обратная связь", 'url_name': 'contact'},
-        {'title': "Войти", 'url_name': 'login'}
-        ]
+from newsline.utils import DataMixin
 
 
-# def index(request):
-#     posts = Post.published.all().select_related('cat')
-#     data = {
-#         'title': 'Главная страница',
-#         'menu': menu,
-#         'posts': posts,
-#         'cat_selected': 0,
-#     }
-#     return render(request, 'newsline/index.html', context=data)
-
-
-class Home(ListView):
+class Home(DataMixin, ListView):
     # model = Post
     template_name = 'newsline/index.html'
     context_object_name = 'posts'
-    extra_context = {
-        'title': 'Главная страница',
-        'menu': menu,
-        'cat_selected': 0,
-    }
+    title_page = 'Главная страница'
+    cat_selected = 0
 
     def get_queryset(self):
         return Post.published.all().select_related('cat')
@@ -50,21 +31,10 @@ def about(request):
     else:
         form = UploadFileForm()
     return render(request, 'newsline/about.html',
-                  {"title": 'О сайте', 'menu': menu, 'form': form})
+                  {"title": 'О сайте', 'form': form})
 
 
-# def show_post(request, post_slug):
-#     post = get_object_or_404(Post, slug=post_slug)
-#     data = {
-#         'title': post.title,
-#         'menu': menu,
-#         'post': post,
-#         'cat_selected': 1,
-#     }
-#     return render(request, 'newsline/post.html', data)
-
-
-class ShowPost(DetailView):
+class ShowPost(DataMixin, DetailView):
     model = Post
     template_name = 'newsline/post.html'
     slug_url_kwarg = 'post_slug'
@@ -72,28 +42,13 @@ class ShowPost(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = context['post'].title
-        context['menu'] = menu
-        return context
+        return self.get_mixin_context(context, title=context['post'].title)
 
     def get_object(self, queryset=None):
         return get_object_or_404(Post.published, slug=self.kwargs[self.slug_url_kwarg])
 
 
-# def show_category(request, cat_slug):
-#     category = get_object_or_404(Category, slug=cat_slug)
-#     posts = Post.published.filter(cat_id=category.pk).select_related('cat')
-#
-#     data = {
-#         'title': f'Категория: {category.name}',
-#         'menu': menu,
-#         'posts': posts,
-#         'cat_selected': category.pk,
-#     }
-#     return render(request, 'newsline/index.html', context=data)
-
-
-class PostCategory(ListView):
+class PostCategory(DataMixin, ListView):
     template_name = 'newsline/index.html'
     context_object_name = 'posts'
     allow_empty = False
@@ -104,10 +59,7 @@ class PostCategory(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         cat = context['posts'][0].cat
-        context['title'] = 'Категория - ' + cat.name
-        context['menu'] = menu
-        context['cat_selected'] = cat.pk
-        return context
+        return self.get_mixin_context(context, title='Категория - ' + cat.name, cat_selected=cat.pk)
 
 
 def login(request):
@@ -118,89 +70,25 @@ def contact(request):
     return HttpResponse('Обратная связь')
 
 
-# def addpage(request):
-#     if request.method == 'POST':
-#         form = AddPostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             # print(form.cleaned_data)
-#             # try:
-#             #    Post.objects.create(**form.cleaned_data)
-#             #    return redirect('home')
-#             # except:
-#             #    form.add_error(None, 'Ошибка добавления записи')
-#             form.save()
-#             return redirect('home')
-#     else:
-#         form = AddPostForm()
-#
-#     data = {
-#         'menu': menu,
-#         'title': 'Добавление статьи',
-#         'form': form
-#     }
-#     return render(request, 'newsline/addpage.html', data)
-class AddPost(CreateView):
+class AddPost(DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'newsline/addpage.html'
-    extra_context = {
-        'title': 'Добавление статьи',
-        'menu': menu,
-    }
+    title_page = 'Добавление статьи'
 
 
-class UpdatePage(UpdateView):
+class UpdatePage(DataMixin, UpdateView):
     model = Post
     fields = ['title', 'content', 'photo', 'is_published', 'cat']
     template_name = 'newsline/addpage.html'
-    extra_context = {
-        'title': 'Редактирование статьи',
-        'menu': menu,
-    }
+    title_page = 'Редактирование статьи'
     success_url = reverse_lazy('home')
 
-# class AddPost(View):  # заменяю функцию представления на класс
-#     def get(self, request):
-#         form = AddPostForm()
-#         data = {
-#             'menu': menu,
-#             'title': 'Добавление статьи',
-#             'form': form
-#         }
-#         return render(request, 'newsline/addpage.html', data)
-#
-#     def post(self, request):
-#         form = AddPostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('home')
-#         data = {
-#             'menu': menu,
-#             'title': 'Добавление статьи',
-#             'form': form
-#         }
-#         return render(request, 'newsline/addpage.html', data)
 
-
-# Create your views here.
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 
-# def show_tag_postlist(request, tag_slug):
-#     tag = get_object_or_404(TagPost, slug=tag_slug)
-#     posts = tag.tags.filter(is_published=Post.Status.PUBLISHED).select_related('cat')
-#
-#     data = {
-#         'title': f'Тег: {tag.tag}',
-#         'menu': menu,
-#         'posts': posts,
-#         'cat_selected': None,
-#     }
-#
-#     return render(request, 'newsline/index.html', context=data)
-
-
-class TagPostList(ListView):
+class TagPostList(DataMixin, ListView):
     template_name = 'newsline/index.html'
     context_object_name = 'posts'
     allow_empty = False
@@ -208,10 +96,8 @@ class TagPostList(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         tag = TagPost.objects.get(slug=self.kwargs['tag_slug'])
-        context['title'] = 'Тег: ' + tag.tag
-        context['menu'] = menu
-        context['cat_selected'] = None
-        return context
+        return self.get_mixin_context(context, title='Тег: ' + tag.tag)
+
 
     def get_queryset(self):
         return Post.published.filter(tags__slug=self.kwargs['tag_slug']).select_related('cat')
